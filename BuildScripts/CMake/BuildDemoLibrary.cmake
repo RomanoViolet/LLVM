7f09486084a227f0c3a18ca30f4824a8cbbc32ca
RomanoViolet <19875252+RomanoViolet@users.noreply.github.com>
2020-05-21 01:57:43 +0200

    Docker Image Works.

--- BuildScripts/CMake/BuildDemoLibrary.cmake
+++ BuildScripts/CMake/BuildDemoLibrary.cmake
@@ -8,12 +8,12 @@ function(buildCPPProject)
   message("CPPProject Sources: " ${CPPProject_SOURCES})
 
   # System Headers
-  include_directories("/usr/lib/llvm-8/include")
+  include_directories("/usr/lib/llvm-11/include")
 
   # Library dependencies: headers
   include_directories(${PATH_TO_COPY_BOUNDEDTYPES_SOURCES})
   include_directories(${PROJECT_SOURCE_DIR}/CoreFunctions)
-  link_directories("/usr/lib/llvm-8/lib/")
+  link_directories("/usr/lib/llvm-11/lib/")
   add_executable(CPPProject "${CPPProject_SOURCES}")
   target_link_libraries(CPPProject clang)
   set_target_properties(CPPProject PROPERTIES LINKER_LANGUAGE "CXX")
