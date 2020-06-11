f66c3ef1684fd75d50ba89e948ea43eafdaa7a81
RomanoViolet <19875252+RomanoViolet@users.noreply.github.com>
2020-06-11 23:49:04 +0200

    Updates.

--- CoreFunctions/Application/StateMachine.hpp
+++ CoreFunctions/Application/StateMachine.hpp
@@ -1,12 +1,24 @@
 #ifndef _STATEMACHINE_HPP_
 #define _STATEMACHINE_HPP_
 
+#include <map>
+#include <string>
+#include <vector>
 namespace RomanoViolet
 {
   class StateMachine
   {
   public:
-    StateMachine( ) = default;
+    /**
+     * @brief Construct a new State Machine object
+     *
+     * @param initialClass: The class from AST which is to be examined.
+     */
+    StateMachine( const std::string initialClass ) : _classToInspect( initialClass )
+    {
+    }
+
+    // All possible states of the state machine.
     enum class State : short {
       INIT,
       NAMESPACE_COLLECTION,
@@ -18,13 +30,48 @@ namespace RomanoViolet
       IOTYPE_COLLECTION
     };
 
+    // Explicitly agreed events in order to transition between states.
     enum class Event : short {
-
+      NAMESPACE,
+      CLASS_DECLARATION,
+      BASECLASS_SPECIFIER,
+      TYPEREF,
+      CXX_ACCESS_SPECIFIER,
+      FIELD_DECLARATION,
+      NAME_SPACE_REFERENCE,
+      TEMPLATE_REFERENCE
     };
 
     void advanceStateMachine( const Event event );
     void doInStateAction( );
 
-  }  // class StateMachine;
+  private:
+    State _currentState;
+    const std::string _classToInspect;
+    void computeTransition( std::string astLabel );
+
+    struct IODetails {
+      std::string _ioName;
+      std::string _type;
+      std::string _direction;
+    };
+
+    struct ClassDetails {
+      std::string _name;
+      std::string _namespace = "";
+      std::string _baseclass;
+      std::vector< IODetails > io;
+    };
+
+    ClassDetails _classDetails;
+
+    // sugar
+    using _currentState_t = State;
+    using _newState_t = State;
+
+    // ruleset: _currentState >--- Event ----> _newState
+    std::map< _currentState_t, std::vector< Event, _newState_t > > rules;
+
+  };  // class StateMachine;
 }  // namespace RomanoViolet
 #endif  // !_STATEMACHINE_HPP_
